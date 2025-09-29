012f97167f5f114dce0e0ac098388715116f2b64
SaharaVC <ms.scalloway2006@gmail.com>
2025-09-28 21:18:49 -0400

    Update TeamProj3.cpp
    class test

--- TeamProj3.cpp
+++ TeamProj3.cpp
@@ -98,7 +98,44 @@ public:
 
 int main()
 {
-    // main function
+    // test personType
+cout << "----- personType Test -----" << endl;
+personType p1("Sahara", "Calloway");
+cout << "Person: ";
+p1.print();
+
+p1.setName("Jane", "Doe");
+cout << "\nUpdated person: ";
+p1.print();
+cout << endl;
+
+// test doctorType
+cout << "\n---- doctorType Test ----" << endl;
+doctorType d1("Gregory", "House", "Pathologist");
+cout << "Doctor: ";
+d1.print();
+cout << "\nSpecialty: " << d1.getSpecialty() << endl;
+
+d1.setSpecialty("Neurologist");
+cout << "Updated Specialty: " << d1.getSpecialty() << endl;
+
+// test billType
+cout << "---- \nbillType test ----" << endl;
+billType b1(101, 150.75, 500.00, 1200.25);
+
+cout << "Bill:" << endl;
+b1.displayBill();
+cout << endl;
+
+// update charges
+b1.setPharmacyCharges(200.50);
+b1.setDoctorFee(600.00);
+b1.setRoomCharges(1300.00);
+
+cout << "Updated Bill:" << endl;
+b1.displayBill();
+
+return 0;
 }
 
 void personType::print() const
@@ -141,42 +178,4 @@ string doctorType::getSpecialty() const
 doctorType::doctorType(string first, string last, string s) : personType(first, last)
 {
     specialty = s;
-}
-
-// test personType
-cout << "----- personType Test -----" << endl;
-personType p1("Calloway", "Sahara");
-cout << "Person: ";
-p1.print();
-
-p1.setName("Doe", "Jane");
-cout << "\nUpdated person: ";
-
-// test doctorType
-cout << "---- doctorType Test ----" << endl;
-doctorType d1("House", "Gregory", "Pathologist");
-cout << "Name: "";
-d1.print();
-cout << "\nSpeciality: " << d1.getSpeciality << endl;
-
-d1.setSpecialty("Neurologist");
-cout << "Updated Specialty: " << d1.getSpecialty() << endl;
-
-// test billType
-cout << "---- billType test ----" << endl;
-billType b1(101, 150.75, 500.00, 1200.25);
-
-cout << "Bill:" << endl;
-b1.displayBill();
-cout << endl;
-
-// Update charges
-b1.setPharmacyCharges(200.50);
-b1.setDoctorFee(600.00);
-b1.setRoomCharges(1300.00);
-
-cout << "Updated Bill:" << endl;
-b1.displayBill();
-
-return 0;
 }
